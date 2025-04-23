0ae1ceb0ac1b787e8b6e1d63115d413957b29781
Khannible <khannible@unomaha.edu>
2025-04-22 23:04:09 -0500

    Update television.py

--- television.py
+++ television.py
@@ -5,19 +5,23 @@ class Television:
     MAX_CHANNEL = 3
 
     def __init__(self):
+        """Initialize the TV with power off, unmuted, min volume, and min channel."""
         self.__status = False
         self.__muted = False
         self.__volume = Television.MIN_VOLUME
         self.__channel = Television.MIN_CHANNEL
 
     def power(self):
+        """Toggle the power status of the TV."""
         self.__status = not self.__status
 
     def mute(self):
+        """Toggle mute status if the TV is on."""
         if self.__status:
             self.__muted = not self.__muted
 
     def channel_up(self):
+        """Increase the channel by one, wrap around if at max, only if TV is on."""
         if self.__status:
             if self.__channel == Television.MAX_CHANNEL:
                 self.__channel = Television.MIN_CHANNEL
@@ -25,6 +29,7 @@ class Television:
                 self.__channel += 1
 
     def channel_down(self):
+        """Decrease the channel by one, wrap around if at min, only if TV is on."""
         if self.__status:
             if self.__channel == Television.MIN_CHANNEL:
                 self.__channel = Television.MAX_CHANNEL
@@ -32,6 +37,7 @@ class Television:
                 self.__channel -= 1
 
     def volume_up(self):
+        """Increase the volume by one if not at max and TV is on. Unmute if muted."""
         if self.__status:
             if self.__muted:
                 self.__muted = False
@@ -39,6 +45,7 @@ class Television:
                 self.__volume += 1
 
     def volume_down(self):
+        """Decrease the volume by one if not at min and TV is on. Unmute if muted."""
         if self.__status:
             if self.__muted:
                 self.__muted = False
@@ -46,5 +53,7 @@ class Television:
                 self.__volume -= 1
 
     def __str__(self):
+        """Return the string representation of the TV's current state."""
         vol = 0 if self.__muted else self.__volume
         return f'Power = {self.__status}, Channel = {self.__channel}, Volume = {vol}'
+
